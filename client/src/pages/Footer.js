a9447b776f5011dff7ca53ff78938bcd7cf31aa6
GWANGUIAN <bbaa3218@gmail.com>
2021-11-26 22:30:30 +0900

    Design: Footer 사이즈 조절(#39)

--- client/src/pages/Footer.js
+++ client/src/pages/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <div className='footer-container'>
       <div className='box-footer'>
-        <img alt='logo' src='img/logo_white.svg' />
+        <img alt='logo' src='img/logo_white.svg' onClick={()=>{window.location.replace('/')}}/>
         <div className='slogan'>나의 헬스 메이트, 쉘 위 헬스</div>
         <div className='creator'>Creator</div>
         <ul>
