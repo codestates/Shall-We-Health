22faf3427ec9bfa1dbcd1b2c79b383dca674ec16
GWANGUIAN <bbaa3218@gmail.com>
2021-11-27 19:10:32 +0900

    Mockup: MyPage 목업 구현(#38)
    마이페이지 탭메뉴 구현
    활동로그 스크롤 및 매치/취소 여부에 따른 삼항연산자 구현
    
    Close codestates/Shall-We-Health#38

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './MyPage.css';
+import Mylog from '../components/MyPage/MyLog'
+import ModifyUserInfo from '../components/MyPage/ModifyUserInfo'
 
 export default function MyPage() {
-  return <div className='mypage-container'>MyPage</div>;
+
+  const [ tap, setTap ] = useState(0)
+
+  return (
+  <div className='mypage-container'>
+    <div className="mypage-title">
+        <img alt="logo" src="img/symbol.svg" />
+        <div>마이 페이지</div>
+      </div>
+      <div className='mypage-tap'>
+        <span onClick={()=>{setTap(false)}} className={!tap ? 'selected' : ''}>활동 로그</span>
+        <span onClick={()=>{setTap(true)}} className={tap ? 'selected' : ''}>내 정보 수정</span>
+      </div>
+      <div className='box-pages'>
+        {!tap ? <Mylog/> : <ModifyUserInfo/>}
+      </div>
+  </div>
+  )
 }
