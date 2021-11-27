9479dcd2c3cc76890112512e11d632d350203f1b
sohhyeonkim <sohhyeonkim@gmail.com>
2021-11-28 01:00:10 +0900

    Feat: 회원가입 페이지 기능구현(#19)
    이메일과 닉네임 중복확인 기능 추가함.
    회원가입 기능 추가함.

--- server/controllers/user/signup.js
+++ server/controllers/user/signup.js
@@ -1,6 +1,34 @@
+const crypto = require("crypto");
+const { user } = require("../../models");
+
 module.exports = async (req, res) => {
   try {
-    res.end();
+    const { nickname, email, password, isOauth, isAdmin } = req.body;
+    const salt = crypto.randomBytes(128).toString("base64");
+    const hashPassword = crypto
+      .createHash("sha512")
+      .update(password + salt)
+      .digest("hex");
+    const data = await User.create({
+      nickname,
+      email,
+      oauth,
+      salt,
+      password: hashPassword,
+      isOauth,
+      isAdmin,
+    });
+    if (data) {
+      return res.status(201).end();
+    } else {
+      return res.status(400).json({
+        data: null,
+        error: {
+          path: "/user/signup",
+          message: "Insufficient body data",
+        },
+      });
+    }
   } catch (err) {
     throw err;
   }
