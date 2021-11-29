45e3b7ea693020bb99124721849c710def5989a6
moonseonyeong <seon_moon@naver.com>
2021-11-29 22:41:31 +0900

    MockUp: Login 로그인페이지에서 모달변경 및 목업 CSS 수정

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -4,6 +4,7 @@ import './Login.css';
 export default function Login() {
   const [loginInfo, setLoginInfo] = useState({ email: "", pw: "" })
   const [failModal, setFailModal] = useState(false)
+  const [modal, setModal] = useState(false)
 
 
   const valueChange = (e) => {
@@ -16,34 +17,38 @@ export default function Login() {
   }
 
 
+
   return (
-    <div className="login-container">
-      <div className="login-inner-contanier">
-        <div className="home-logo"></div>
-        <div className="inputEmail-text">Email</div>
-        <div className="email-underbar"></div>
-        <div><input className="input-UserInfo" name="email" onChange={valueChange} placeholder="이메일을 입력하세요" /></div>
-        <div className="inputPw-text">Password</div>
-        <div className="pw-underbar"></div>
-        <div><input className="input-UserInfo" name="pw" onChange={valueChange} placeholder="비밀번호를 입력하세요" onKeyPress={EnterLogin} style={{ marginBottom: '20px' }} /></div>
+    <>
+      {modal === true
+        ? (
 
-
-        <div><button className="btn-login" onClick={() => { setFailModal(true) }}> 로그인 </button></div>
-
-        {failModal === true
-          ? <EmailVerification setFailModal={setFailModal} />
-          : ""
-        }
-        <div className="fail-login" > 이메일 또는 비밀번호가 잘못 입력 되었습니다. </div>
-
-        <div className="under-grid">
-          <div className="grid-item"> 회원가입 </div>
-          <div className="grid-item">  네이버 </div>
-          <div className="grid-item"> 카카오톡 </div>
-          <div className="grid-item">  비밀번호찾기 </div>
-        </div>
-      </div>
-    </div>
+          <div className="login-container">
+            <div className="login-modal fadeIn">
+              <div className='btn-loginClose' onClick={() => { setModal(false) }}></div>
+              <div className="home-logo"></div>
+              <div className="text-email">Email</div>
+              <input className="input-email" name="email" onChange={valueChange} placeholder='Email' />
+              <div className="text-pw">Password</div>
+              <input className="input-pw" name="pw" type='password' onChange={valueChange} placeholder='Password' onKeyPress={EnterLogin} />
+              <div className="text-falilogin" > 이메일 또는 비밀번호가 잘못 입력 되었습니다 </div>
+              <button className="btn-login" onClick={() => { setFailModal(true) }}> 로그인 </button>
+              {failModal === true
+                ? <EmailVerification setFailModal={setFailModal} />
+                : ""
+              }
+              <div className="grid-func">
+                <div className="grid-signup"> 회원가입 </div>
+                <div className="grid-naver">  네이버 </div>
+                <div className="grid-kakao"> 카카오톡 </div>
+                <div className="grid-findpw">  비밀번호 찾기 </div>
+              </div>
+            </div>
+          </div>
+        )
+        : <button onClick={() => { setModal(true) }}>로그인</button>
+      }
+    </>
   );
 
 }
@@ -56,7 +61,7 @@ function EmailVerification({ setFailModal }) {
   return (
     <div className="modal-container">
       <div className="box-modal">
-        <div className="modal-message">입력하신 메일로 <br /> 회원가입 인증을 진행 해주세요</div>
+        <div className="modal-message">입력하신 메일로<br />회원가입인증을 진행해주세요</div>
         <div>
           <span className="btn-close" onClick={() => { setFailModal(false) }}>확인</span>
         </div>
