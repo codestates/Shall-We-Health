c04acb53275f2cedd53e717bbdde3e1cc9a97c78
sohhyeonkim <sohhyeonkim@gmail.com>
2021-11-30 01:48:07 +0900

    Feat: 로그인,로그아웃 기능구현(#17)
    로그인 성공 시 jwt로 accessToke생성하여 쿠키에 전달.
    로그아웃 시 쿠키에 저장된 토큰 삭제.
    소셜 로그인 시 accessToken 생성 payload에 이메일,닉네임,생성및변경날짜만 포함.
    jsonwebtoken 모듈 설치함.

--- server/controllers/user/kakao.js
+++ server/controllers/user/kakao.js
@@ -1,7 +1,36 @@
+const jwt = require("jsonwebtoken");
+const { User } = require("../../models");
+
 module.exports = async (req, res) => {
+  const { email } = req.body;
   try {
-    res.end();
+    const loginData = await User.findOne({
+      where: { email },
+      attributes: ["email", "nickname", "createdAt", "updatedAt"],
+    });
+    if (loginData) {
+      const accessToken = jwt.sign(
+        loginData.dataValues,
+        process.env.ACCESS_SECRET,
+        {
+          expiresIn: "1h",
+        }
+      );
+      res
+        .cookie("accessToken", accessToken, {
+          maxAge: 6 * 10 * 60, //1시간
+        })
+        .status(200)
+        .json({
+          data: {
+            accessToken,
+            userData: userData.dataValues,
+          },
+        });
+    } else {
+    }
   } catch (err) {
+    console.log(err);
     throw err;
   }
 };
