409cc2b58da24d3de3c1e293452d2d788caf2dcd
lim-yubin <yubineric@gmail.com>
2021-11-30 17:45:44 +0900

    MockUp: View페이지 목업구현
    View.js 목업구현
    
    Issue #37

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import View from './pages/View';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import FindPw from './pages/FindPw';
-import FindParter from './pages/FindPartner';
+import FindPartner from './pages/FindPartner';
 import Board from './pages/Board';
 import MyPage from './pages/MyPage';
 import AdminPage from './pages/AdminPage';
@@ -37,7 +37,7 @@ function App() {
         <Route path='/find-pw' component={FindPw} />
         <Route path='/footer' component={Footer} />
         <Route path='/board' component={Board} />
-        <Route path='/find-partner' component={FindParter} />
+        <Route path='/find-partner' component={FindPartner} />
         <Route path='/view' component={View} />
         <Route path='/mypage' component={MyPage} />
         <Route path='/admin' component={AdminPage} />
