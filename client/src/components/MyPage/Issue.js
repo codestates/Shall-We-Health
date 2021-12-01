d01460947d9d79b989ed26c17cc3b02057b75ca0
GWANGUIAN <bbaa3218@gmail.com>
2021-12-01 20:57:59 +0900

    Feat: 마이페이지 활동로그 기능 구현(#113)
    활동로그 조회 및 취소 완료 매칭 text decoration 구현
    추천/취소 기능 구현
    신고하기 기능 구현
    
    Close codestates/Shall-We-Health#113

--- client/src/components/MyPage/Issue.js
+++ client/src/components/MyPage/Issue.js
@@ -22,6 +22,9 @@ export default function Issue({setIssue, issueInfo}) {
         content,
         postId: issueInfo.postId
       })
+      .then(()=>{
+        setCompleteModal(true)
+      })
     }
   }
 
