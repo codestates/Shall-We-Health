5396b25323627077925a3b54c7ce243392693075
sohhyeonkim <sohhyeonkim@gmail.com>
2021-12-07 00:24:43 +0900

    Feat:페이지네이션 기능구현(#15)
    버튼 클릭시 paginate 함수 동작 이벤트 추가함.
    필요한 전체 페이지 개수, 한번에 보일수 있는 최대 페이지개수, 실제로 필요한 페이지 개수 정의함.

--- client/src/components/Pagination/Pagination.js
+++ client/src/components/Pagination/Pagination.js
@@ -1,15 +1,23 @@
 import './Pagination.css';
 
 const Pagination = ({
-  //   activePage,
+  activePage,
   itemsCountPerPage,
   totalItemsCount,
   pageRangeDisplayed,
   paginate,
 }) => {
-  const activePage = 1;
-  const pageNumbers = [1, 2, 3, 4, 5];
-  for (let i = 0; i <= Math.ceil(totalItemsCount / itemsCountPerPage); i++) {
+  const pageNumbers = [];
+  const totalPage = Math.ceil(totalItemsCount / itemsCountPerPage); //필요한 모든 페이지 페이지 개수
+  const maxPage = pageRangeDisplayed; //한번에 보일 수 있는 최대 페이지 개수
+  let pages = 0; //필요한 페이지 개수
+  if (totalPage < maxPage) {
+    pages = totalPage;
+  } else {
+    pages = maxPage;
+  }
+
+  for (let i = 0; i <= pages; i++) {
     pageNumbers.push(i);
   }
   return (
@@ -18,10 +26,21 @@ const Pagination = ({
         <span
           className='first'
           style={{ color: '#C4C4C4', fontWeight: 'bold' }}
+          onClick={() => {
+            activePage > pageRangeDisplayed
+              ? paginate(activePage - pageRangeDisplayed)
+              : paginate(1);
+          }}
         >
           &lt;&lt;
         </span>
-        <span className='prev' style={{ color: '#C4C4C4', fontWeight: 'bold' }}>
+        <span
+          className='prev'
+          style={{ color: '#C4C4C4', fontWeight: 'bold' }}
+          onClick={() => {
+            activePage > 1 ? paginate(activePage - 1) : paginate(activePage);
+          }}
+        >
           &lt;
         </span>
         <ul className='paging-group'>
@@ -35,10 +54,22 @@ const Pagination = ({
             );
           })}
         </ul>
-        <span className='next' style={{ color: '#C4C4C4', fontWeight: 'bold' }}>
+        <span
+          className='next'
+          style={{ color: '#C4C4C4', fontWeight: 'bold' }}
+          onClick={() => {
+            activePage < totalPage
+              ? paginate(activePage + 1)
+              : paginate(activePage);
+          }}
+        >
           &gt;
         </span>
-        <span className='last' style={{ color: '#C4C4C4', fontWeight: 'bold' }}>
+        <span
+          className='last'
+          style={{ color: '#C4C4C4', fontWeight: 'bold' }}
+          onClick={() => paginate(totalPage)}
+        >
           &gt;&gt;
         </span>
       </div>
