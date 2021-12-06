122a46dea992c4b22d171f684b6e88353e271f4d
GWANGUIAN <bbaa3218@gmail.com>
2021-12-06 11:49:18 +0900

    Design: 네비게이션 바 반응형 웹 구현(#152)
    width에 따른 메이트 탐색 및 찾기 버튼 수정
    
    close #152

--- client/src/pages/NavigationBar.js
+++ client/src/pages/NavigationBar.js
@@ -1,26 +1,28 @@
 import React, { useState } from 'react';
 import Login from './Login';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faThList, faUser, faSearch, fas } from '@fortawesome/free-solid-svg-icons';
 import './NavigationBar.css';
 
 export default function NavigationBar() {
-  const [isLogin, setIsLogin] = useState(true);
-  const [isAdmin, setIsAdmin] = useState(true);
+  const { isLogin, isAdmin } = useSelector((state) => state.loginReducer);
 
   const [loginModal, setLoginModal] = useState(false)
 
   return (
     <div className='navBar-container'>
       <div className='bar'>
-        <img alt='logo' src='img/logo.svg' className='home-logo' onClick={()=>{window.location.replace('/')}}/>
+        <img alt='logo' src='img/logo.svg' className='home-logo web' onClick={()=>{window.location.replace('/')}}/>
+        <img alt='logo' src='img/symbol.svg' className='home-logo mobile' onClick={()=>{window.location.replace('/')}}/>
         <div className='btn-board'>
         <Link
               to='/board'
               style={{ color: 'inherit', textDecoration: 'inherit' }}
             >
-          <span>메이트 탐색</span>
+          <span  className='web'>메이트 탐색</span>
+          <FontAwesomeIcon className='board mobile' icon={faThList} />
           </Link>
         </div>
         <div className='btn-search'>
@@ -28,7 +30,8 @@ export default function NavigationBar() {
               to='/find-partner'
               style={{ color: 'inherit', textDecoration: 'inherit' }}
             >
-          <span>메이트 모집</span>
+          <span className='web'>메이트 모집</span>
+          <FontAwesomeIcon className='find-partner mobile' icon={faSearch} />
           </Link>
         </div>
         {isLogin ? (
@@ -38,7 +41,8 @@ export default function NavigationBar() {
               to='/mypage'
               style={{ color: 'inherit', textDecoration: 'inherit' }}
             >
-              <span>마이페이지</span>
+              <span className='web'>마이페이지</span>
+              <FontAwesomeIcon className='mypage mobile' icon={faUser} />
               </Link>
             </div>
             <div className='btn-logout'>
