3874a58850bde0515f140fc8cf910b19c55baffe
lim-yubin <yubineric@gmail.com>
2021-12-08 04:04:03 +0900

    Feat: view페이지 기능구현(1)
    1.게시물별 지도에 마커 띄워주기
    2.게시물별 sbd/bodypart/message/nickname/address 표시
    3.게시물의 isMatched에 따라 버튼(신청하기/취소하기/마감)분기하여 표시
    
    *추가구현필요*
    1.수정,삭제
    2.socket.io
    3.버튼별 요청보내기
    
    issue #41

--- client/src/components/FindPartner/FindMap.js
+++ client/src/components/FindPartner/FindMap.js
@@ -13,6 +13,7 @@ export default function FindMap(
     e.preventDefault();
     setPlace(inputText);
     setInputText("");
+    setMarkerPlace({})
   };
 
 
--- client/src/pages/View.js
+++ client/src/pages/View.js
@@ -1,21 +1,86 @@
 /* global kakao */
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import FindMap from '../components/FindPartner/FindMap';
-
 import './View.css';
 
-export default function View() {
+
+export default function View({ match }) {
+  const postNumber = match.params.postId
+  const [sbdResult, setSbdResult] = useState([])
+  const [bodypartArr, setBodypartArr] = useState([])
+  const [messageResult, setMessageResult] = useState('')
+  const [nickname, setNickname] = useState('')
+  const [loca, setLoca] = useState('')
+  const [reserveDate, setReserveDate] = useState('')
+  const [searchPlace, setSearchPlace] = useState('')
+  const [ismatched, setIsMatched] = useState('')
+
+
+
+  const getDetailpost = () => {
+    axios.get(`${process.env.REACT_APP_SERVER_API}/post/${postNumber}`)
+      .then((res) => {
+        setSbdResult(res.data.data[0].description.sbd)
+        setBodypartArr(res.data.data[0].description.bodyPart)
+        setMessageResult(res.data.data[0].description.message)
+        setNickname(res.data.data[0].hostNickname)
+        setLoca(res.data.data[0].location.road_address_name)
+        setReserveDate(res.data.data[0].reserved_at)
+        setSearchPlace(res.data.data[0].location)
+        setIsMatched(res.data.data[0].isMatched)
+        // console.log(res.data.data)
+      }
+      )
+  }
+
+  function MakeBodyPartButton({ el }) {
+    return (
+      <button className='view-body-options'>{el}</button>
+    )
+  }
+
+  function copyClick() {
+    navigator.clipboard.writeText(loca)
+  }
+
+  useEffect(() => {
+    getDetailpost()
+  }, [])
+
 
 
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667),
-      level: 3
+      center: new kakao.maps.LatLng(searchPlace.y, searchPlace.x),
+      level: 2
     };
-
     const map = new kakao.maps.Map(container, options);
-  }, []);
+    const ps = new kakao.maps.services.Places();
+    let infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+    function displayMarker(place) {
+      let marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(place.y, place.x)
+      });
+      kakao.maps.event.addListener(marker, 'click', function () {
+        // 마커를 클릭하면 장소명이 인포윈도우에 표출
+        infowindow.setContent('<div style="padding:5px;font-size:12px">' + place.place_name + '</div>');
+        infowindow.open(map, marker);
+      });
+    }
+    displayMarker(searchPlace)
+  }, [searchPlace]);
+
+
+
+
+
+
+
+
+
 
   return <div className='view-container'>
     <div id='map' style={{ width: '100%', height: '45vh', zIndex: 0 }}></div>
@@ -34,41 +99,46 @@ export default function View() {
             </div>
           </div>
 
-
           <div className='bodypart-result'>
             <div className='bodypart-title'>운동부위</div>
-            <button className='view-body-options'>가슴</button>
-            <button className='view-body-options'>삼두</button>
-            <button className='view-body-options'>복근</button>
-            <button className='view-body-options'>유산소</button>
+            {
+              bodypartArr.map((el, id) => {
+                return (
+                  <MakeBodyPartButton key={id} el={el} />
+                )
+              })
+            }
           </div>
 
           <div className='weight-result'>
             <div className='weight-title'>3대 운동 중량</div>
-            <button className='view-weight-options'>200~300kg</button>
+            <button className='view-weight-options'>{sbdResult}</button>
           </div>
 
           <div className='message-result'>
             <div className='message-title'>파트너에게 한마디</div>
-            <div className='message-content'>조지죠.</div>
+            <div className='message-content'>{messageResult}</div>
           </div>
         </div>
 
         <div className='info-container'>
-
-          <div className='info-user-nickname'>형광언더아머 님</div>
+          <div className='info-user-nickname'>{nickname} 님</div>
           <div className='date-address-section'>
-            <div className='info-date'>2021년 11월 30일 화요일 11:00</div>
+            <div className='info-date'>
+              {`${reserveDate.slice(0, 4)}년 
+            ${reserveDate.slice(5, 7)}월
+            ${reserveDate.slice(8, 10)}일
+            ${reserveDate.slice(11, 13)}:${reserveDate.slice(14, 16)}`}
+            </div>
             <div className='address-section'>
-              <div className='info-address'>경기도 안산시 어쩌구</div>
-              <div className='address-copy'>주소 복사</div>
+              <div className='info-address'>{loca}</div>
+              <div className='address-copy' onClick={copyClick}>주소 복사</div>
             </div>
           </div>
           <div className='button-section'>
-            <button className='finding-button'>모집중</button>
-            <button className='application-button'>신청하기</button>
-            <button className='deadline-button'>마감</button>
-            <button className='match-cancel-button'>매칭취소</button>
+            {(ismatched === false) ? <button className='application-button'>신청하기</button>
+              : (ismatched === 2) ? <button className='match-cancel-button'>매칭취소</button>
+                : <button className='deadline-button'>마감</button>}
           </div>
         </div>
 
