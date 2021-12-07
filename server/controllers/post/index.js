1dc444f011b01aa46e5696dc7e58d4001e9e892f
sohhyeonkim <sohhyeonkim@gmail.com>
2021-12-07 17:10:20 +0900

    Fix: 파트너찾기 페이지 invalid value 에러해결(#213)

--- server/controllers/post/index.js
+++ server/controllers/post/index.js
@@ -6,7 +6,29 @@ module.exports = {
     try {
       const { userId, reserved_at, location, description } = req.body;
       if (userId && reserved_at && location && description) {
-        const postData = Post.create({
+        //동일한 날짜에 이미 업로드(호스트) 또는 신청한(게스트) 내역이 있다면, 신청제한
+        const existingData = await Post.findAll({
+          where: {
+            [Op.and]: [
+              sequelize.where(
+                sequelize.fn("date", sequelize.col("reserved_at")),
+                ">=",
+                reserved_at.slice(0, 10)
+              ),
+              sequelize.where(
+                sequelize.fn("date", sequelize.col("reserved_at")),
+                "<=",
+                reserved_at.slice(0, 10)
+              ),
+            ],
+            [Op.or]: [{ hostId: userId }, { guestId: userId }],
+            [Op.not]: [{ isMatched: 2 }],
+          },
+        });
+        if (existingData.length !== 0) {
+          return res.status(204).end();
+        }
+        await Post.create({
           hostId: userId,
           reserved_at,
           location,
