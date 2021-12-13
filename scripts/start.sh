e5122661f69408d2515553d16b6a4d8184d1d704
sohhyeonkim <sohhyeonkim@gmail.com>
2021-12-13 17:59:31 +0900

    Chore: start.sh 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -11,4 +11,5 @@ export ACCOUNT_USER=$(aws ssm get-parameters --region ap-northeast-2 --names ACC
 export ACCOUNT_PASS=$(aws ssm get-parameters --region ap-northeast-2 --names ACCOUNT_PASS --query Parameters[0].Value | sed 's/"//g')
 export PORT =$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
 
+
 authbind --deep pm2 start index.js
