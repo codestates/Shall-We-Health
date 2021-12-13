2bd8f5565fcf781cd62e82599e521b6532d24d07
sohhyeonkim <sohhyeonkim@gmail.com>
2021-12-13 18:01:38 +0900

    Chore:start.sh 띄어쓰기 삭제

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ export DB_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DB_NAME 
 export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
 export ACCOUNT_USER=$(aws ssm get-parameters --region ap-northeast-2 --names ACCOUNT_USER --query Parameters[0].Value | sed 's/"//g')
 export ACCOUNT_PASS=$(aws ssm get-parameters --region ap-northeast-2 --names ACCOUNT_PASS --query Parameters[0].Value | sed 's/"//g')
-export PORT =$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
+export PORT=$(aws ssm get-parameters --region ap-northeast-2 --names PORT --query Parameters[0].Value | sed 's/"//g')
 
 
 authbind --deep pm2 start index.js
