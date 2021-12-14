15f6957178c68b143e1c490da5e6e4ea6ee09ea4
GWANGUIAN <bbaa3218@gmail.com>
2021-12-14 15:53:09 +0900

    Feat: OAuth 회원가입 닉네임 중복확인 및 랜덤 문자열 추가(#287)

--- server/controllers/user/naver.js
+++ server/controllers/user/naver.js
@@ -3,14 +3,14 @@ const { User } = require("../../models");
 const axios = require("axios");
 
 module.exports = async (req, res) => {
-    const { token } = req.body
-    const userData = await axios.get('https://openapi.naver.com/v1/nid/me', {
-        headers : {
-            'Authorization' : `Bearer ${token}`,
-        }
-    })
-    const { email, nickname } = userData.data.response;
-    const isOauth = 1
+  const { token } = req.body;
+  const userData = await axios.get("https://openapi.naver.com/v1/nid/me", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const { email, nickname } = userData.data.response;
+  const isOauth = 1;
 
   try {
     const loginData = await User.findOne({
@@ -18,20 +18,51 @@ module.exports = async (req, res) => {
       attributes: ["email", "createdAt"],
     });
 
-
     if (loginData) {
       /* 회원가입이 되어있을 때 */
 
-      const accessToken = jwt.sign(loginData.dataValues, process.env.ACCESS_SECRET, { expiresIn: "1h" });
+      const accessToken = jwt.sign(
+        loginData.dataValues,
+        process.env.ACCESS_SECRET,
+        { expiresIn: "1h" }
+      );
 
-      res.cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 10000 }) // 1시간
+      res
+        .cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 10000 }) // 1시간
         .status(200)
         .end();
     } else {
       /* 회원가입 안되어있을 때 */
-      const newLoginData = await User.create({ email, isOauth, nickname, });
-      const accessToken = jwt.sign(newLoginData.dataValues, process.env.ACCESS_SECRET, { expiresIn: "1h" });
-      res.cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 10000 }) // 1시간
+      function makeId(length) {
+        var result = "";
+        var characters =
+          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+          result += characters.charAt(
+            Math.floor(Math.random() * charactersLength)
+          );
+        }
+        return result;
+      }
+
+      let duplication = true
+      while(duplication){
+        var randomSet = await makeId(5)
+        duplication = await User.findOne({
+          where: { nickname: nickname+randomSet },
+          attributes: ["email", "createdAt"],
+        })
+      }
+      
+      const newLoginData = await User.create({ email, isOauth, nickname : nickname+randomSet, isEmailVerified:1 });
+      const accessToken = jwt.sign(
+        newLoginData.dataValues,
+        process.env.ACCESS_SECRET,
+        { expiresIn: "1h" }
+      );
+      res
+        .cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 10000 }) // 1시간
         .status(201)
         .end();
     }
