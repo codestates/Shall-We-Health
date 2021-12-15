4e5ae4ad700c8256dc7b57105dedb7eddc883953
lim-yubin <yubineric@gmail.com>
2021-12-15 18:50:56 +0900

    Fixx: 시간버그 수정시도

--- server/models/index.js
+++ server/models/index.js
@@ -16,6 +16,7 @@ if (config.use_env_variable) {
     host: config.host,
     port: config.port,
     dialect: config.dialect,
+    timezone: "+09:00",
     dialectOptions: {
       charset: "utf8mb4",
       dateStrings: true,
