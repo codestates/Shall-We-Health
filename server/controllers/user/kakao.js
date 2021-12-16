4c08dff052d1b177468fd3201e1f963c528e6946
sohhyeonkim <sohhyeonkim@gmail.com>
2021-12-16 14:11:09 +0900

    Feat: 소셜로그인 에러처리 및 utils > social.js 추가(#321)
    model의 user.js에서 ondelete cascade 속성으로 변경함.

--- server/controllers/user/kakao.js
+++ server/controllers/user/kakao.js
@@ -1,9 +1,18 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../../models");
+const { socialSignup } = require("../../utils/social");
 
 module.exports = async (req, res) => {
   const { email, nickname, isOauth } = req.body;
-
+  if (!email || !nickname || !isOauth) {
+    return res.status(400).json({
+      data: null,
+      error: {
+        path: "user/kakao",
+        mesage: "Insufficient body data",
+      },
+    });
+  }
   try {
     const loginData = await User.findOne({
       where: { email },
@@ -12,7 +21,6 @@ module.exports = async (req, res) => {
 
     if (loginData) {
       /* 회원가입이 되어있을 때 */
-
       const accessToken = jwt.sign(
         loginData.dataValues,
         process.env.ACCESS_SECRET,
@@ -20,38 +28,46 @@ module.exports = async (req, res) => {
       );
 
       res
-        .cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 1000 * 3 }) // 3시간
+        .cookie("accessToken", accessToken, {
+          maxAge: 6 * 10 * 60 * 1000 * 3,
+        }) // 3시간
         .status(200)
         .end();
     } else {
       /* 회원가입 안되어있을 때 */
-      function makeId(length) {
-        var result = "";
-        var characters =
-          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-          );
-        }
-        return result;
+      const socialUserData = await socialSignup(5, nickname, email, isOauth);
+      if (socialUserData) {
+        return res.status(201).end();
       }
-      let duplication = true;
-      while (duplication) {
-        var randomSet = await makeId(5);
-        duplication = await User.findOne({
-          where: { nickname: nickname + randomSet },
-          attributes: ["email", "createdAt"],
-        });
-      }
-      await User.create({
-        email,
-        isOauth,
-        nickname: nickname + randomSet,
-        isEmailVerified: 1,
-      });
-      return res.status(201).end();
+      //   function makeId(length) {
+      //     let result = "";
+      //     let characters =
+      //       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      //     let charactersLength = characters.length;
+      //     for (let i = 0; i < length; i++) {
+      //       result += characters.charAt(
+      //         Math.floor(Math.random() * charactersLength)
+      //       );
+      //     }
+      //     return result;
+      //   }
+      //   let duplication = true;
+      //   let randomSet = "";
+      //   while (duplication) {
+      //     randomSet = makeId(5);
+      //     duplication = await User.findOne({
+      //       where: { nickname: nickname + randomSet },
+      //       attributes: ["email", "createdAt"],
+      //     });
+      //   }
+      //   await User.create({
+      //     email,
+      //     isOauth,
+      //     nickname: nickname + randomSet,
+      //     isEmailVerified: 1,
+      //   });
+      //   return res.status(201).end();
+      // }
     }
   } catch (err) {
     console.log(err);
--- server/controllers/user/naver.js
+++ server/controllers/user/naver.js
@@ -1,9 +1,19 @@
 const jwt = require("jsonwebtoken");
 const { User } = require("../../models");
+const { socialSignup } = require("../../utils/social");
 const axios = require("axios");
 
 module.exports = async (req, res) => {
   const { token } = req.body;
+  if (!token) {
+    return res.status(400).json({
+      data: null,
+      error: {
+        path: "user/naver",
+        message: "Insufficient body data",
+      },
+    });
+  }
   const userData = await axios.get("https://openapi.naver.com/v1/nid/me", {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -20,7 +30,6 @@ module.exports = async (req, res) => {
 
     if (loginData) {
       /* 회원가입이 되어있을 때 */
-
       const accessToken = jwt.sign(
         loginData.dataValues,
         process.env.ACCESS_SECRET,
@@ -33,41 +42,43 @@ module.exports = async (req, res) => {
         .end();
     } else {
       /* 회원가입 안되어있을 때 */
-      function makeId(length) {
-        var result = "";
-        var characters =
-          "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        var charactersLength = characters.length;
-        for (var i = 0; i < length; i++) {
-          result += characters.charAt(
-            Math.floor(Math.random() * charactersLength)
-          );
-        }
-        return result;
-      }
+      // function makeId(length) {
+      //   let result = "";
+      //   let characters =
+      //     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      //   let charactersLength = characters.length;
+      //   for (let i = 0; i < length; i++) {
+      //     result += characters.charAt(
+      //       Math.floor(Math.random() * charactersLength)
+      //     );
+      //   }
+      //   return result;
+      // }
+      // let duplication = true;
+      // let randomSet = "";
+      // while (duplication) {
+      //   randomSet = makeId(5);
+      //   duplication = await User.findOne({
+      //     where: { nickname: randomSet },
+      //     attributes: ["email", "createdAt"],
+      //   });
+      // }
 
-      let duplication = true;
-      while (duplication) {
-        var randomSet = await makeId(5);
-        duplication = await User.findOne({
-          where: { nickname: randomSet },
-          attributes: ["email", "createdAt"],
-        });
-      }
-
-      const newLoginData = await User.create({
-        email,
-        isOauth,
-        nickname: randomSet,
-        isEmailVerified: 1,
-      });
+      // const newLoginData = await User.create({
+      //   email,
+      //   isOauth,
+      //   nickname: randomSet,
+      //   isEmailVerified: 1,
+      // });
+      const newLoginData = await socialSignup(5, "", email, isOauth);
+      console.log(newLoginData);
       const accessToken = jwt.sign(
         newLoginData.dataValues,
         process.env.ACCESS_SECRET,
-        { expiresIn: "1h" }
+        { expiresIn: "3h" }
       );
       res
-        .cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 10000 }) // 1시간
+        .cookie("accessToken", accessToken, { maxAge: 6 * 10 * 60 * 1000 * 3 }) // 3시간
         .status(201)
         .end();
     }
