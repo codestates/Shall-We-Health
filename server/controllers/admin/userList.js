6439ab008ff97add1f1400f65ccb91033a8653cd
sohhyeonkim <sohhyeonkim@gmail.com>
2022-01-09 15:01:53 +0900

    Refactor: admin, mypage 에러 status code 변경

--- server/controllers/admin/userList.js
+++ server/controllers/admin/userList.js
@@ -5,10 +5,11 @@ module.exports = async (req, res) => {
   try {
     const { page, keyword } = req.query;
     const limit = 7;
-    const offset = (page - 1) * limit;
+    const indexOfLastPost = page * limit;
+    const indexOfFirstPost = indexOfLastPost - limit;
 
     if (!page) {
-      return res.status(404).json({
+      return res.status(400).json({
         data: null,
         error: "/admin/user-list",
         message: "Insufficient query",
@@ -18,8 +19,6 @@ module.exports = async (req, res) => {
       const sql =
       "select Users.id, Users.email, Users.nickname, IFNULL(A.hostNum,0) AS hostNum, IFNULL(B.guestNum,0) AS guestNum from Users LEFT JOIN (SELECT hostId, COUNT(id) AS hostNum FROM Posts GROUP BY hostId) as A ON Users.id=A.hostId LEFT JOIN (SELECT guestId, COUNT(id) AS guestNum FROM Posts GROUP BY guestId) AS B ON Users.id=B.guestId Where Users.nickname like ? OR Users.email like ?";
     const rows = await sequelize.query(sql, { replacements: [`%${keyword}%`,`%${keyword}%`],type: QueryTypes.SELECT });
-    const indexOfLastPost = page * limit;
-    const indexOfFirstPost = indexOfLastPost - limit;
     const currentPosts = rows.slice(indexOfFirstPost, indexOfLastPost);
 
     if (rows) {
@@ -33,51 +32,10 @@ module.exports = async (req, res) => {
         },
       });
     }
-  }
-    // if (keyword) {
-    //   const { count, rows } = await User.findAndCountAll({
-    //     where: {
-    //       [Op.or]: [
-    //         { email: { [Op.substring]: keyword } },
-    //         { nickname: { [Op.substring]: keyword } },
-    //       ],
-    //     },
-    //     attributes: [
-    //       "id",
-    //       "email",
-    //       "nickname",
-    //       [
-    //         sequelize.fn("count", sequelize.col("`hosts`.`hostId`")),
-    //         "hostNum",
-    //       ],
-    //       [
-    //         sequelize.fn("count", sequelize.col("`guests`.`isMatched`")),
-    //         "guestNum",
-    //       ],
-    //     ],
-    //     group: ["User.id"],
-
-    //     include: [
-    //       { model: Post, as: "hosts", attributes: [] },
-    //       {
-    //         model: Post,
-    //         as: "guests",
-    //         attributes: [],
-    //         where: {
-    //           isMatched: 1,
-    //         },
-    //         required: false,
-    //       },
-    //     ],
-    //   });
-    //   return res.status(200).json({ data: rows, count: rows.length });
-    // } 
-    else {
+  }else {
       const sql =
         "select Users.id, Users.email, Users.nickname, IFNULL(A.hostNum,0) AS hostNum, IFNULL(B.guestNum,0) AS guestNum from Users LEFT JOIN (SELECT hostId, COUNT(id) AS hostNum FROM Posts GROUP BY hostId) as A ON Users.id=A.hostId LEFT JOIN (SELECT guestId, COUNT(id) AS guestNum FROM Posts GROUP BY guestId) AS B ON Users.id=B.guestId";
       const rows = await sequelize.query(sql, { type: QueryTypes.SELECT });
-      const indexOfLastPost = page * limit;
-      const indexOfFirstPost = indexOfLastPost - limit;
       const currentPosts = rows.slice(indexOfFirstPost, indexOfLastPost);
 
       if (rows) {
