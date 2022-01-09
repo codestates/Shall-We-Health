26032b4de5ea6d0180f8dac723dc6d6f361969c1
sohhyeonkim <sohhyeonkim@gmail.com>
2022-01-09 14:40:42 +0900

    Refactor: Admin 서버버그해결 및 이벤트 추가(#350)
    userList.js에서 키워드로 필터링하는 부분 에러해결함.
    10분마다 자동으로 isMatched=2로 변경하는 autoCancel 생성함.

--- server/controllers/admin/userList.js
+++ server/controllers/admin/userList.js
@@ -15,43 +15,64 @@ module.exports = async (req, res) => {
       });
     }
     if (keyword) {
-      const { count, rows } = await User.findAndCountAll({
-        where: {
-          [Op.or]: [
-            { email: { [Op.substring]: keyword } },
-            { nickname: { [Op.substring]: keyword } },
-          ],
-        },
-        attributes: [
-          "id",
-          "email",
-          "nickname",
-          [
-            sequelize.fn("count", sequelize.col("`hosts`.`hostId`")),
-            "hostNum",
-          ],
-          [
-            sequelize.fn("count", sequelize.col("`guests`.`isMatched`")),
-            "guestNum",
-          ],
-        ],
-        group: ["User.id"],
+      const sql =
+      "select Users.id, Users.email, Users.nickname, IFNULL(A.hostNum,0) AS hostNum, IFNULL(B.guestNum,0) AS guestNum from Users LEFT JOIN (SELECT hostId, COUNT(id) AS hostNum FROM Posts GROUP BY hostId) as A ON Users.id=A.hostId LEFT JOIN (SELECT guestId, COUNT(id) AS guestNum FROM Posts GROUP BY guestId) AS B ON Users.id=B.guestId Where Users.nickname like ? OR Users.email like ?";
+    const rows = await sequelize.query(sql, { replacements: [`%${keyword}%`,`%${keyword}%`],type: QueryTypes.SELECT });
+    const indexOfLastPost = page * limit;
+    const indexOfFirstPost = indexOfLastPost - limit;
+    const currentPosts = rows.slice(indexOfFirstPost, indexOfLastPost);
 
-        include: [
-          { model: Post, as: "hosts", attributes: [] },
-          {
-            model: Post,
-            as: "guests",
-            attributes: [],
-            where: {
-              isMatched: 1,
-            },
-            required: false,
-          },
-        ],
-      });
-      return res.status(200).json({ data: rows, count: rows.length });
+    if (rows) {
+      return res.status(200).json({ data: currentPosts, count: rows.length });
     } else {
+      return res.status(404).json({
+        data: null,
+        error: {
+          path: "/admin/user-list",
+          message: "page not found",
+        },
+      });
+    }
+  }
+    // if (keyword) {
+    //   const { count, rows } = await User.findAndCountAll({
+    //     where: {
+    //       [Op.or]: [
+    //         { email: { [Op.substring]: keyword } },
+    //         { nickname: { [Op.substring]: keyword } },
+    //       ],
+    //     },
+    //     attributes: [
+    //       "id",
+    //       "email",
+    //       "nickname",
+    //       [
+    //         sequelize.fn("count", sequelize.col("`hosts`.`hostId`")),
+    //         "hostNum",
+    //       ],
+    //       [
+    //         sequelize.fn("count", sequelize.col("`guests`.`isMatched`")),
+    //         "guestNum",
+    //       ],
+    //     ],
+    //     group: ["User.id"],
+
+    //     include: [
+    //       { model: Post, as: "hosts", attributes: [] },
+    //       {
+    //         model: Post,
+    //         as: "guests",
+    //         attributes: [],
+    //         where: {
+    //           isMatched: 1,
+    //         },
+    //         required: false,
+    //       },
+    //     ],
+    //   });
+    //   return res.status(200).json({ data: rows, count: rows.length });
+    // } 
+    else {
       const sql =
         "select Users.id, Users.email, Users.nickname, IFNULL(A.hostNum,0) AS hostNum, IFNULL(B.guestNum,0) AS guestNum from Users LEFT JOIN (SELECT hostId, COUNT(id) AS hostNum FROM Posts GROUP BY hostId) as A ON Users.id=A.hostId LEFT JOIN (SELECT guestId, COUNT(id) AS guestNum FROM Posts GROUP BY guestId) AS B ON Users.id=B.guestId";
       const rows = await sequelize.query(sql, { type: QueryTypes.SELECT });
